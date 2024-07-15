de48e9e3b7cb5435ecfea15f50f1e37e62543176
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-15 19:12:09 +0000

    Contador y botones hechos y funcionando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
+import React from "react";
+import Counter from "./Contador";
+
 
 
 //create your first component
 const Home = () => {
 
-	const [Count, setCount] = useState(0)
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setCount(prevCount => prevCount + 1);
-		}, 1000);
-
-		return () => clearInterval(interval);
-
-	}, []);
-	const formatCount = (Count) => {
-		return Count.toString().padStart(6, '0');
-	};
-
 	return (
-		<div className="text-center">
-			<Container>
-				<Row className='justify-content-center align-items-center h-100'>
-					<Col xs='auto'>
-						<h1 >{formatCount(Count)}</h1>
-					</Col>
-				</Row>
-			</Container>
-		</div>
-	);
+		<>
+			<Counter />
+		</>
+	)
 };
 
 export default Home;
